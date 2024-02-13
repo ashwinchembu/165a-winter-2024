baba59735eeb3a0605da143f44b3c9679864dce3
mirastenger <mira@makaimedia.com>
2024-02-13 22:37:41 +0000

    test

--- __main__.py
+++ __main__.py
@@ -10,12 +10,12 @@ query = Query(grades_table)
 keys = []
 
 insert_time_0 = time()
-for i in range(0, 10000):
+for i in range(0, 1000000):
     query.insert(906659671 + i, 93, 0, 0, 0)
     keys.append(906659671 + i)
 insert_time_1 = time()
 
-print("Inserting 10k records took:  \t\t\t", insert_time_1 - insert_time_0)
+print("Inserting 100k records took:  \t\t\t", insert_time_1 - insert_time_0)
 
 # Measuring update Performance
 update_cols = [
