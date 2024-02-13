df5924028bba06be989b946f382c566c92fb9ec0
mirastenger <mira@makaimedia.com>
2024-02-13 12:45:29 -0800

    Update table.cpp

--- lstore/table.cpp
+++ lstore/table.cpp
@@ -219,7 +219,7 @@ RID Table::update(RID rid, const std::vector<int>& columns) {
         }
     }
     i--;
-		auto iter = page_directory.find(rid);
+		auto iter = page_directory.find(rid.id);
 		*(iter->second.pointers[0]) = rid_id;
 		RID new_rid = (page_range[i].get())->update(rid, rid_id, columns);
 		page_directory.insert({rid_id, new_rid});
