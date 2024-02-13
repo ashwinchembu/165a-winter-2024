741e767b07fdab7475826853dc7b5136a48f15e7
Trevorwl <95387595+Trevorwl@users.noreply.github.com>
2024-02-13 14:21:57 -0800

    Original python testers

--- __main__.py
+++ __main__.py
@@ -1,6 +1,6 @@
 from lstore.py.db import Database
 from lstore.py.query import Query
-from time import time
+from time import process_time
 from random import choice, randrange
 
 # Student Id and 4 grades
@@ -9,11 +9,11 @@ grades_table = db.create_table('Grades', 5, 0)
 query = Query(grades_table)
 keys = []
 
-insert_time_0 = time()
+insert_time_0 = process_time()
 for i in range(0, 10000):
     query.insert(906659671 + i, 93, 0, 0, 0)
     keys.append(906659671 + i)
-insert_time_1 = time()
+insert_time_1 = process_time()
 
 print("Inserting 10k records took:  \t\t\t", insert_time_1 - insert_time_0)
 
@@ -26,31 +26,31 @@ update_cols = [
     [None, None, None, None, randrange(0, 100)],
 ]
 
-update_time_0 = time()
+update_time_0 = process_time()
 for i in range(0, 10000):
     query.update(choice(keys), *(choice(update_cols)))
-update_time_1 = time()
+update_time_1 = process_time()
 print("Updating 10k records took:  \t\t\t", update_time_1 - update_time_0)
 
 # Measuring Select Performance
-select_time_0 = time()
+select_time_0 = process_time()
 for i in range(0, 10000):
     query.select(choice(keys),0 , [1, 1, 1, 1, 1])
-select_time_1 = time()
+select_time_1 = process_time()
 print("Selecting 10k records took:  \t\t\t", select_time_1 - select_time_0)
 
 # Measuring Aggregate Performance
-agg_time_0 = time()
+agg_time_0 = process_time()
 for i in range(0, 10000, 100):
     start_value = 906659671 + i
     end_value = start_value + 100
     result = query.sum(start_value, end_value - 1, randrange(0, 5))
-agg_time_1 = time()
+agg_time_1 = process_time()
 print("Aggregate 10k of 100 record batch took:\t", agg_time_1 - agg_time_0)
 
 # Measuring Delete Performance
-delete_time_0 = time()
+delete_time_0 = process_time()
 for i in range(0, 10000):
     query.delete(906659671 + i)
-delete_time_1 = time()
+delete_time_1 = process_time()
 print("Deleting 10k records took:  \t\t\t", delete_time_1 - delete_time_0)
