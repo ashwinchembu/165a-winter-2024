76c65bcc5a4d9f25cffec8b25ca29fd0e88bf7e5
zhao-18 <zhao1455@gmail.com>
2024-02-13 20:20:52 -0800

    g

--- lstore/page.cpp
+++ lstore/page.cpp
@@ -239,12 +239,12 @@ RID PageRange::update(RID rid, int rid_new, const std::vector<int>& columns) {
         while (latest_rid != (*((page_range[latest_page * num_column + 1].second)->data + latest_offset*sizeof(int)))) {
             latest_offset++;
         }
+        std::cout << "hhh" << latest_rid != (*((page_range[latest_page * num_column + 1].second)->data + latest_offset*sizeof(int))) << std::endl;
     }
     std::vector<int> latest_record(num_column);
     for (int i = 0; i < num_column; i++) {
         latest_record[i] = (*((page_range[latest_page * num_column + i].second)->data + latest_offset*sizeof(int)));
         std::cout << latest_record[i] << std::endl;
-        std::cout << "num_column" << num_column << std::endl;
     }
 
     int schema_encoding = 0;
