14bbb72bfdea759231275475d417e98e339938e6
mirastenger <mira@makaimedia.com>
2024-02-13 16:21:24 -0800

    Update query.cpp

--- lstore/query.cpp
+++ lstore/query.cpp
@@ -156,7 +156,7 @@ int Query::sum_version(int start_range, int end_range, int aggregate_column_inde
         if (rids[i].id != 0) { //If RID is valid i.e. not deleted
             if (rids[i].check_schema(aggregate_column_index)) { // if this column is changed
                 int indirection = *((rids[i]).pointers[0]); // the new indirection
-                for (int j = 1; j <= relative_version + 1; j++) {
+                for (int j = 1; j < relative_version; j++) {
                     indirection = *(table->page_directory.find(indirection)->second.pointers[0]); //get the next indirection
 										if(indirection > 0){
 											break;
