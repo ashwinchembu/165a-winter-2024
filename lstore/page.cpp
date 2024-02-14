a9ddc19516b760259377b2f33a71943129a1aba7
zhao-18 <zhao1455@gmail.com>
2024-02-13 21:16:28 -0800

    s

--- lstore/page.cpp
+++ lstore/page.cpp
@@ -238,9 +238,9 @@ RID PageRange::update(RID rid, int rid_new, const std::vector<int>& columns) {
     } else {
         while (latest_offset < NUM_SLOTS && latest_rid != (*((page_range[latest_page * num_column + 1].second)->data + latest_offset*sizeof(int)))) {
             latest_offset++;
-            std::cout << "latest_offset" << latest_offset << std::endl;
+            std::cout << "latest_offset " << latest_offset << std::endl;
             std::cout << "First rid in the latest page " << page_range[latest_page * num_column].first.id << std::endl;
-            std::cout << "latest_rid" << latest_rid << std::endl;
+            std::cout << "latest_rid " << latest_rid << std::endl;
         }
     }
     std::vector<int> latest_record(num_column);
@@ -248,6 +248,7 @@ RID PageRange::update(RID rid, int rid_new, const std::vector<int>& columns) {
         latest_record[i] = (*((page_range[latest_page * num_column + i].second)->data + latest_offset*sizeof(int)));
     }
     std::cout << "latest_offset " << latest_offset << std::endl;
+    std::cout << "tail_last " << tail_last << std::endl;
 
     int schema_encoding = 0;
     std::vector<int*> new_record(num_column);
