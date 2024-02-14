8d0bb74a334c9b3259edb0c5c82721c752e957b9
mirastenger <mira@makaimedia.com>
2024-02-14 07:11:30 +0000

    last

--- lstore/page.h
+++ lstore/page.h
@@ -10,7 +10,7 @@
 class Page {
 public:
     /// @TODO Move this to config file
-    constexpr static int PAGE_SIZE = 2048; // bytes
+    constexpr static int PAGE_SIZE = 4096; // bytes
     constexpr static int NUM_SLOTS = PAGE_SIZE/sizeof(int); // bytes
     int num_rows = 0;
     int availability[NUM_SLOTS] = {0}; // 0 is empty, 1 is occupied, 2 is deleted.
@@ -26,8 +26,8 @@ class PageRange {
 public:
     /* data */
     /// @TODO Move this to config file
-    const int PAGE_SIZE = 2048;
-    const int LOGICAL_PAGE = 4;
+    const int PAGE_SIZE = 4096;
+    const int LOGICAL_PAGE = 16;
     const int NUM_SLOTS = 2048*LOGICAL_PAGE;
     int num_slot_left = NUM_SLOTS;
     int base_last = 0;
