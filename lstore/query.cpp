b1897a9ae67b16a09f7851cb352df7fc39bcbab1
mirastenger <mira@makaimedia.com>
2024-02-13 21:36:08 -0800

    Update query.cpp

--- lstore/query.cpp
+++ lstore/query.cpp
@@ -133,10 +133,11 @@ bool Query::update(int primary_key, const std::vector<int>& columns) {
     RID update_rid = table->update(base_rid, columns); // insert update into the table
     std::vector<int> old_columns;
 		std::vector<int> new_columns;
-		std::cout << "num columns: " << table->num_columns << '\n';
     for(int i = 0; i < table->num_columns; i++){ // fill old_columns with the contents of previous update
       old_columns.push_back(*(last_update.pointers[i + 4]));
+			std::cout << "old: " << (*(last_update.pointers[i + 4])) << '\n';
 			new_columns.push_back(*(update_rid.pointers[i + 4]));
+			std::cout << "new: " << (*(update_rid.pointers[i + 4])) << '\n';
     }
     if(update_rid.id != 0){
         table->index->update_index(update_rid, new_columns, old_columns); //update the index
