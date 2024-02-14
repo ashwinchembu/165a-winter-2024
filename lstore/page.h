f6767e718ebdc6a6ccb9bb502ad6069ca9df6f3b
zhao-18 <zhao1455@gmail.com>
2024-02-13 18:32:31 -0800

    Page size updated

--- lstore/page.h
+++ lstore/page.h
@@ -10,7 +10,7 @@
 class Page {
 public:
     /// @TODO Move this to config file
-    constexpr static int PAGE_SIZE = 4096; // bytes
+    constexpr static int PAGE_SIZE = 4096000; // bytes
     constexpr static int NUM_SLOTS = PAGE_SIZE/sizeof(int); // bytes
     int num_rows = 0;
     int availability[NUM_SLOTS] = {0}; // 0 is empty, 1 is occupied, 2 is deleted.
@@ -26,8 +26,8 @@ class PageRange {
 public:
     /* data */
     /// @TODO Move this to config file
-    const int PAGE_SIZE = 4096;
-    const int NUM_SLOTS = 16384;
+    const int PAGE_SIZE = 4096000;
+    const int NUM_SLOTS = 16276000;
     int num_slot_left = NUM_SLOTS;
     int base_last = 0;
     int tail_last = 0;
