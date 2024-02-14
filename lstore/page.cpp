1405090af4144cdc751d131c2d6a721944db7074
zhao-18 <zhao1455@gmail.com>
2024-02-13 19:15:14 -0800

    test

--- lstore/page.cpp
+++ lstore/page.cpp
@@ -221,11 +221,10 @@ RID PageRange::update(RID rid, int rid_new, const std::vector<int>& columns) {
         new_tail = true;
     }
     int latest_page = page_of_rid;
-    std::cout << "expr" << std::endl;
     if (latest_rid < 0) { // Are there any updates we should be aware of?
         int latest_page = base_last + 1;
         for (; latest_page <= tail_last; latest_page++) { //Look for the page row
-            if (new_tail || latest_page == tail_last) {
+            if (new_tail && latest_page == tail_last) {
                 break;
             } else if (page_range[latest_page * num_column].first.id < latest_rid) {
                 break;
