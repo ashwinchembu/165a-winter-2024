65ffd6b06b13cfc5c514cb041721c9f9fa8ac77a
zhao-18 <zhao1455@gmail.com>
2024-02-25 21:47:40 -0800

    54

--- lstore/index.cpp
+++ lstore/index.cpp
@@ -164,7 +164,7 @@ void Index::create_index(const int& column_number) {
             int value;
             int indirection_num = buffer_pool.get(rid, INDIRECTION_COLUMN);
 
-            if (buffer_pool.get(rid, SCHEMA_ENCODING_COLUMN >> (column_number - 1)) & (0b1)) { // If the column of the record at loc is updated
+            if ((buffer_pool.get(rid, SCHEMA_ENCODING_COLUMN) >> (column_number - 1)) & (0b1)) { // If the column of the record at loc is updated
                 RID update_rid = table->page_directory.find(indirection_num)->second;
                 value = buffer_pool.get(update_rid, column_number + NUM_METADATA_COLUMNS);
             } else {
