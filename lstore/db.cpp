9e0ab56d66f1e06b8a8a5dada4f2b9a0942e84e0
mirastenger <mira@makaimedia.com>
2024-02-27 12:46:56 -0800

    ??

--- lstore/db.cpp
+++ lstore/db.cpp
@@ -208,13 +208,13 @@ void Database::write(){
  * @return Table Return the newly created table
  *
  */
-Table* Database::create_table(const std::string& name, const int& num_columns, const int& key_index){
+Table Database::create_table(const std::string& name, const int& num_columns, const int& key_index){
   Table* table = new Table(name, num_columns, key_index);
   auto insert = tables->insert(std::make_pair(name, table));
   if (insert.second == false) {
     throw std::invalid_argument("A table with this name already exists in the database. The table was not added.");
   }
-  return table;
+  return *table;
 }
 
 /***
--- lstore/db.h
+++ lstore/db.h
@@ -13,7 +13,7 @@ class Database{
     ~Database();
     void open(const std::string& path);
     void close();
-    Table* create_table(const std::string& name, const int& num_columns, const int& key_index);
+    Table create_table(const std::string& name, const int& num_columns, const int& key_index);
     void drop_table(const std::string& name);
     Table get_table(const std::string& name);
     std::string file_path = "./ECS165";
