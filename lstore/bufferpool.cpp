80f856cc1f744385b9ea00257214216d0d8d3cdf
mirastenger <mira@makaimedia.com>
2024-02-27 23:43:07 -0800

    Update bufferpool.cpp

--- lstore/bufferpool.cpp
+++ lstore/bufferpool.cpp
@@ -98,8 +98,8 @@ Frame* BufferPool::search(const RID& rid, const int& column){
   Frame* range_end = (hash == hash_vector.size() - 1) ? tail : hash_vector[hash + 1]->prev; //end of hash range
   Frame* current_frame = range_begin; //iterate through range
   while(current_frame != range_end->next){
-    std::cout << "we are looking at " << current_frame->first_rid_page << " and " << current_frame->column << std::endl;
-    
+  //  std::cout << "we are looking at " << current_frame->first_rid_page << " and " << current_frame->column << std::endl;
+
     if ((current_frame->valid)) {
       if(rid.first_rid_page == current_frame->first_rid_page && column == current_frame->column){
         return current_frame;
