7c178b91a413c5e83e2c9b1f44a349dee9003e6b
Trevorwl <95387595+Trevorwl@users.noreply.github.com>
2024-03-10 22:53:58 -0700

    Add files via upload

--- lstore/py/transaction.py
+++ lstore/py/transaction.py
@@ -8,8 +8,8 @@ class Transaction:
     # Creates a transaction object.
     """
     def __init__(self):
-        self.queries = []
-        pass
+        # self.queries = []
+        self.selfPtr = Transaction_constructor()
 
     """
     # Adds the given query to this transaction
@@ -19,26 +19,66 @@ class Transaction:
     # t.add_query(q.update, grades_table, 0, *[None, 1, None, 2, None])
     """
     def add_query(self, query, table, *args):
-        self.queries.append((query, args))
+        #query is a function and each symbol it is tested against is a 
+        #valid compare because of how the library is built
+        queryCode = query.__code__
+        queryObj = query.__self__
+        
+        if queryCode == Query.insert.__code__:
+            
+            Transaction_add_query_insert(self.selfPtr, queryObj.selfPtr,
+                    table.selfPtr, fillAndReturnIntBuffer(args))
+           
+        elif queryCode == Query.update.__code__:
+            
+            Transaction_add_query_update(self.selfPtr,queryObj.selfPtr,
+                    table.selfPtr,args[0],fillAndReturnIntBuffer(args[1]))
+         
+        elif queryCode == Query.select.__code__:
+            
+            Transaction_add_query_select(self.selfPtr,queryObj.selfPtr,
+                    table.selfPtr,args[0],args[1],fillAndReturnIntBuffer(args[2]))
+      
+        elif queryCode == Query.select_version.__code__:
+            
+            Transaction_add_query_select_version(self.selfPtr,queryObj.selfPtr,
+                    table.selfPtr,args[0],args[1],fillAndReturnIntBuffer(args[2]),
+                    args[3])
+ 
+        elif queryCode == Query.sum.__code__:
+            
+            Transaction_add_query_sum(self.selfPtr,queryObj.selfPtr,table.selfPtr,
+                    args[0],args[1],args[2])
+      
+        elif queryCode == Query.sum_version.__code__:
+            
+            Transaction_add_query_sum_version(self.selfPtr,query.selfPtr,
+            table.selfPtr,args[0],args[1],args[2],args[3])
+     
+        # self.queries.append((query, args))
+        
         # use grades_table for aborting
-
         
     # If you choose to implement this differently this method must still return True if transaction commits or False on abort
     def run(self):
-        for query, args in self.queries:
-            result = query(*args)
-            # If the query has failed the transaction should abort
-            if result == False:
-                return self.abort()
-        return self.commit()
+        pass
+        # for query, args in self.queries:
+        #     result = query(*args)
+        #     # If the query has failed the transaction should abort
+        #     if result == False:
+        #         return self.abort()
+        # return self.commit()
 
     
     def abort(self):
         #TODO: do roll-back and any other necessary operations
-        pass
+        return False
 
     
     def commit(self):
         # TODO: commit to database
-        pass
+        return True
+    
+    def destroyPointer(self):
+        Transaction_destructor(self.selfPtr)
 
