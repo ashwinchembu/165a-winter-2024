128546b1ad8bf7f16771e77b3be2cc925d949110
zhao-18 <zhao1455@gmail.com>
2024-03-11 22:24:30 -0700

    ?

--- lstore/transaction.cpp
+++ lstore/transaction.cpp
@@ -172,9 +172,9 @@ void Transaction::add_query(Query& q, Table& t, int& key, const int& column) {
 bool Transaction::run() {
     bool transaction_completed = true; //any case where transaction does not need to be redone
     bool _commit = true; //any case where transaction does not need to be redone
+    db_log.lk.lock();
     db_log.num_transactions++;
     xact_id = db_log.num_transactions;
-    db_log.lk.lock();
     db_log.entries.insert({xact_id, LogEntry(queries)}); //note in log that transaction has begun
     db_log.lk.unlock();
 
