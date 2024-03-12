9a21b03aa6bc46dfe50c77d293fd89f7777ddb97
zhao-18 <zhao1455@gmail.com>
2024-03-11 17:36:16 -0700

    ????

--- DBWrapper.py
+++ DBWrapper.py
@@ -319,7 +319,7 @@ def c_intOrUnreasonable(number):
 #Elements of None are given an extreme value
 def fillAndReturnIntBuffer(*args):
     lst = list(args)
-    print(args[1])
+    print(args[0])
     erase_buffer_vector()
     for i in lst:
         print(i)
