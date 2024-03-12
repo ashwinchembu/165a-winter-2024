890ff30879d0c5051bf047fa6180f185c38ba280
zhao-18 <zhao1455@gmail.com>
2024-03-12 12:17:01 -0700

    ?

--- lstore/table.cpp
+++ lstore/table.cpp
@@ -72,22 +72,22 @@ RID Table::insert(const std::vector<int>& columns) {
 	record.id = rid_id;
 	// std::lock(insert_lock, page_directory_shared);
 	// std::lock_guard insert_lk(insert_lock);
-	insert_lock.lock();
+	// insert_lock.lock();
 	{
-		page_range_shared.lock();
+		page_range_unique.lock();
 		if (page_range.size() == 0 || !(page_range.back().get()->base_has_capacity())) {
-			page_range_shared.unlock();
+			page_range_unique.unlock();
 			//std::shared_ptr<PageRange>newPageRange{new PageRange(record, columns)};
 			// page_range_unique.lock();
 			//page_range.push_back(newPageRange); // Make a base page with given record
 			// page_range_unique.unlock();
-			insert_lock.unlock();
+			// insert_lock.unlock();
 		} else { // If there are base page already, just insert it normally.
 
 			record.first_rid_page_range = (page_range.back().get())->pages[0].first_rid_page_range;
 			PageRange* prange = (page_range.back().get());
-			page_range_shared.unlock();
-			insert_lock.unlock();
+			page_range_unique.unlock();
+			// insert_lock.unlock();
 
 			//if (prange->insert(record, columns)) {
 			//	return RID(0);
