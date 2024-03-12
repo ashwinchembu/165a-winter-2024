374f6d78a4db9124dc456cc6c862dad30ab534fc
zhao-18 <45818093+zhao-18@users.noreply.github.com>
2024-03-12 10:59:30 -0700

    Update transaction.cpp

--- lstore/transaction.cpp
+++ lstore/transaction.cpp
@@ -176,9 +176,9 @@ bool Transaction::run() {
     db_log.num_transactions++;
 	db_log.lk.unlock();
 
-	db_log.shared_lk.lock();
+	db_log.lk_shared.lock();
     xact_id = db_log.num_transactions;
-	db_log.shared_lk.unlock();
+	db_log.lk_shared.unlock();
 
 	db_log.lk.lock();
     db_log.entries.insert({xact_id, LogEntry(queries)}); //note in log that transaction has begun
