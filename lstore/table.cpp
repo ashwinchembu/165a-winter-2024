89a190372c29e4dde691cda6bb5f400a311cd837
zhao-18 <zhao1455@gmail.com>
2024-03-12 12:56:25 -0700

    IDK

--- lstore/table.cpp
+++ lstore/table.cpp
@@ -62,38 +62,38 @@ Table::Table (const Table& rhs) {
  *
  */
 RID Table::insert(const std::vector<int>& columns) {
-	// insert_lock2.lock();
+	insert_lock2.lock();
 	num_insert++; // Should not need mutex here. num_insert is std::atomic and rid solely depend on that.
 	int rid_id = num_insert;
 	buffer_pool.lock_manager.find(name)->second.insert({rid_id, new LockManagerEntry});
-	// insert_lock2.unlock();
+	insert_lock2.unlock();
 	RID record;
 	record.table_name = name;
 	record.id = rid_id;
 	// std::lock(insert_lock, page_directory_shared);
 	// std::lock_guard insert_lk(insert_lock);
-	insert_lock.lock();
-	{
-		page_range_shared.lock();
-		if (page_range.size() == 0 || !(page_range.back().get()->base_has_capacity())) {
-			page_range_shared.unlock();
-			std::shared_ptr<PageRange>newPageRange{new PageRange(record, columns)};
-			page_range_unique.lock();
-			page_range.push_back(newPageRange); // Make a base page with given record
-			page_range_unique.unlock();
-			insert_lock.unlock();
-		} else { // If there are base page already, just insert it normally.
-
-			record.first_rid_page_range = (page_range.back().get())->pages[0].first_rid_page_range;
-			PageRange* prange = (page_range.back().get());
-			page_range_shared.unlock();
-			insert_lock.unlock();
-
-			if (prange->insert(record, columns)) {
-				return RID(0);
-			}
-		}
-	}
+	// insert_lock.lock();
+	// {
+	// 	page_range_shared.lock();
+	// 	if (page_range.size() == 0 || !(page_range.back().get()->base_has_capacity())) {
+	// 		page_range_shared.unlock();
+	// 		std::shared_ptr<PageRange>newPageRange{new PageRange(record, columns)};
+	// 		page_range_unique.lock();
+	// 		page_range.push_back(newPageRange); // Make a base page with given record
+	// 		page_range_unique.unlock();
+	// 		insert_lock.unlock();
+	// 	} else { // If there are base page already, just insert it normally.
+ //
+	// 		record.first_rid_page_range = (page_range.back().get())->pages[0].first_rid_page_range;
+	// 		PageRange* prange = (page_range.back().get());
+	// 		page_range_shared.unlock();
+	// 		insert_lock.unlock();
+ //
+	// 		if (prange->insert(record, columns)) {
+	// 			return RID(0);
+	// 		}
+	// 	}
+	// }
 
 
 	page_directory_unique.lock();
