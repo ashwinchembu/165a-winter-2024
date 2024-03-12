8dc409392aa4ead207ce7b051454dffe68a9d42b
zhao-18 <zhao1455@gmail.com>
2024-03-11 18:48:57 -0700

    >?

--- lstore/table.cpp
+++ lstore/table.cpp
@@ -66,11 +66,12 @@ RID Table::insert(const std::vector<int>& columns) {
 	RID record;
 	record.table_name = name;
 	record.id = rid_id;
-
-	std::scoped_lock lock{insert_lock, page_directory_shared};/*
-	insert_lock.lock();*/
+	std::cout << "Probably here" << std::endl;
+	std::lock(insert_lock, page_directory_shared);
+	std::cout << "Probably here 2" << std::endl;
+	insert_lock.lock();
 	{
-		// page_range_shared.lock();
+		page_range_shared.lock();
 		if (page_range.size() == 0 || !(page_range.back().get()->base_has_capacity())) {
 			page_range_shared.unlock();
 			std::shared_ptr<PageRange>newPageRange{new PageRange(record, columns)};
