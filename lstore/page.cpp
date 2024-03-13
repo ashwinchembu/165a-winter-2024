a571736c9c634dc8099273c5c8f8d3d73a548b63
zhao-18 <zhao1455@gmail.com>
2024-03-12 23:09:03 -0700

    FIX

--- lstore/page.cpp
+++ lstore/page.cpp
@@ -89,7 +89,6 @@ PageRange::~PageRange(){
  */
 int PageRange::insert(RID& new_rid, const std::vector<int>& columns) {
     // Lock to protect variable in Page range.
-    // mutex_insert.lock();
     std::unique_lock lock(mutex_insert);
     std::unique_lock lock_manager_lock(buffer_pool.lock_manager_lock, std::defer_lock);
 
@@ -100,19 +99,18 @@ int PageRange::insert(RID& new_rid, const std::vector<int>& columns) {
         tail_last++;
         base_last++;
         lock.unlock();
-        // mutex_insert.unlock();
 
         // Update information in the rid class
         new_rid.offset = 0;
         new_rid.first_rid_page = new_rid.id;
+
         // Lock the rid of record that we are inserting
-        // buffer_pool.unique_lock_manager_lock.lock();
         lock_manager_lock.lock();
 
         if (!(buffer_pool.lock_manager.find(new_rid.table_name)->second.find(new_rid.id)->second->unique_lock->try_lock())) {
+            lock_manager_lock.unlock();
             return 1;
         }
-        // buffer_pool.unique_lock_manager_lock.unlock();
         lock_manager_lock.unlock();
         // Write in the metadatas
         buffer_pool.insert_new_page(new_rid, INDIRECTION_COLUMN, new_rid.id);
@@ -125,37 +123,35 @@ int PageRange::insert(RID& new_rid, const std::vector<int>& columns) {
         for (int i = NUM_METADATA_COLUMNS; i < num_column; i++) {
             buffer_pool.insert_new_page(new_rid, i, columns[i - NUM_METADATA_COLUMNS]);
         }
+
         // Unlock the rid of the record once we are done inserting
-        // buffer_pool.unique_lock_manager_lock.lock();
         lock_manager_lock.lock();
         buffer_pool.lock_manager.find(new_rid.table_name)->second.find(new_rid.id)->second->unique_lock->unlock();
-        // buffer_pool.unique_lock_manager_lock.unlock();
         lock_manager_lock.unlock();
+
         // Protecting pages vector from multiple thread writing simultaneously
         std::unique_lock plock(page_lock);
-        // page_lock.lock();
         // Insert the first rid of the logical page into appropriate place
         pages.insert(pages.begin() + base_last, new_rid);
-        // page_lock.unlock();
         plock.unlock();
     } else {
         // Update status of the page range
         base_last_wasfull = (num_slot_used_base == PAGE_SIZE);
         num_slot_used_base++;
         lock.unlock();
-        // mutex_insert.unlock();
+
         // Update information in the rid class
         new_rid.offset = num_slot_used_base - 1;
         new_rid.first_rid_page = pages[base_last].id;
 
         // Lock the rid of record that we are inserting
-        // buffer_pool.unique_lock_manager_lock.lock();
         lock_manager_lock.lock();
         if (!(buffer_pool.lock_manager.find(new_rid.table_name)->second.find(new_rid.id)->second->unique_lock->try_lock())) {
+            lock_manager_lock.unlock();
             return 1;
         }
-        // buffer_pool.unique_lock_manager_lock.unlock();
         lock_manager_lock.unlock();
+
         // Write in the metadatas
         buffer_pool.set(new_rid, INDIRECTION_COLUMN, new_rid.id, true);
         buffer_pool.set(new_rid, RID_COLUMN, new_rid.id, true);
@@ -163,15 +159,15 @@ int PageRange::insert(RID& new_rid, const std::vector<int>& columns) {
         buffer_pool.set(new_rid, SCHEMA_ENCODING_COLUMN, 0, true);
         buffer_pool.set(new_rid, BASE_RID_COLUMN, new_rid.id, true);
         buffer_pool.set(new_rid, TPS, 0, true);
+
         // Write in the data
         for (int i = NUM_METADATA_COLUMNS; i < num_column; i++) {
             buffer_pool.set(new_rid, i, columns[i - NUM_METADATA_COLUMNS], true);
         }
+
         // Unlock the rid of the record once we are done inserting
-        // buffer_pool.unique_lock_manager_lock.lock();
         lock_manager_lock.lock();
         buffer_pool.lock_manager.find(new_rid.table_name)->second.find(new_rid.id)->second->unique_lock->unlock();
-        // buffer_pool.unique_lock_manager_lock.unlock();
         lock_manager_lock.unlock();
     }
     return 0;
@@ -200,9 +196,11 @@ int PageRange::update(RID& rid, RID& rid_new, const std::vector<int>& columns, c
     std::unique_lock mlock(mutex_update);
     std::unique_lock lock_manager_lock(buffer_pool.lock_manager_lock, std::defer_lock);
     std::unique_lock uniq_lock_rid(*(buffer_pool.lock_manager.find(rid_new.table_name)->second.find(rid_new.id)->second->mutex), std::defer_lock);
+
     // Create new tail pages if there are no space left or tail page does not exist.
     // Otherwise just insert the update in the last tail page
     if (tail_last_wasfull) {
+
         // Update the variable of the page range
         tail_last_wasfull = false;
         tail_last++;
@@ -216,12 +214,13 @@ int PageRange::update(RID& rid, RID& rid_new, const std::vector<int>& columns, c
         // Lock the rid of record that we are inserting
         lock_manager_lock.lock();
         if (!(uniq_lock_rid.try_lock())) {
+            lock_manager_lock.unlock();
             return 1;
         }
         lock_manager_lock.unlock();
 
         // Write in the metadata except for schema encoding column
-        buffer_pool.insert_new_page(rid_new, INDIRECTION_COLUMN, rid.id);
+        buffer_pool.insert_new_page(rid_new, INDIRECTION_COLUMN, latest_rid.id);
         buffer_pool.insert_new_page(rid_new, RID_COLUMN, rid_new.id);
         buffer_pool.insert_new_page(rid_new, TIMESTAMP_COLUMN, 0);
         buffer_pool.insert_new_page(rid_new, BASE_RID_COLUMN, rid.id);
@@ -245,13 +244,15 @@ int PageRange::update(RID& rid, RID& rid_new, const std::vector<int>& columns, c
         // Unlock the lock for the new record. If we reach here then we were able to lock it before.
         lock_manager_lock.lock();
         uniq_lock_rid.unlock();
-
         lock_manager_lock.unlock();
+
         // Setting the new RID to be representation of the page if the page was newly created
         std::unique_lock plock(page_lock);
         pages.push_back(rid_new);
         plock.unlock();
+
     } else {
+
         // Update the variable of the page range
         num_slot_used_tail++;
         tail_last_wasfull = (num_slot_used_tail == PAGE_SIZE);
@@ -264,13 +265,14 @@ int PageRange::update(RID& rid, RID& rid_new, const std::vector<int>& columns, c
         // Lock the rid of record that we are inserting
         lock_manager_lock.lock();
         if (!(uniq_lock_rid.try_lock())) {
+            lock_manager_lock.unlock();
             return 1;
         }
         // buffer_pool.unique_lock_manager_lock.unlock();
         lock_manager_lock.unlock();
 
         // Write in the metadata except for schema encoding column
-        buffer_pool.set(rid_new, INDIRECTION_COLUMN, rid.id, true);
+        buffer_pool.set(rid_new, INDIRECTION_COLUMN, latest_rid.id, true);
         buffer_pool.set(rid_new, RID_COLUMN, rid_new.id, true);
         buffer_pool.set(rid_new, TIMESTAMP_COLUMN, 0, true);
         buffer_pool.set(rid_new, BASE_RID_COLUMN, rid.id, true);
@@ -292,15 +294,19 @@ int PageRange::update(RID& rid, RID& rid_new, const std::vector<int>& columns, c
         buffer_pool.set(rid_new, SCHEMA_ENCODING_COLUMN, schema_encoding, true);
 
         // Unlock the lock for the new record. If we reach here then we were able to lock it before.
+        lock_manager_lock.lock();
         uniq_lock_rid.unlock();
+        lock_manager_lock.unlock();
     }
 
     // Updating indirection column and schema encoding column for the base page
     buffer_pool.pin(latest_rid, SCHEMA_ENCODING_COLUMN, 'X');
-    buffer_pool.set(latest_rid, INDIRECTION_COLUMN, rid_new.id, false);
     buffer_pool.unpin(latest_rid, SCHEMA_ENCODING_COLUMN, 'X');
+
     int base_schema = buffer_pool.get(rid, SCHEMA_ENCODING_COLUMN);
+
     buffer_pool.pin(rid, SCHEMA_ENCODING_COLUMN, 'X');
+    buffer_pool.set(rid, INDIRECTION_COLUMN, rid_new.id, false);
     buffer_pool.set(rid, SCHEMA_ENCODING_COLUMN, base_schema | schema_encoding, false);
     buffer_pool.unpin(rid, SCHEMA_ENCODING_COLUMN, 'X');
     return 0;
