29aadaee5633914ff65cc9947cce74687e735df7
zhao-18 <zhao1455@gmail.com>
2024-03-14 15:30:24 -0700

    Revert "test"
    This reverts commit f2d0e5de6acd0256240181918602e33562410944.

--- lstore/page.h
+++ lstore/page.h
@@ -48,7 +48,7 @@ public:
     PageRange (RID& new_rid, const std::vector<int>& columns);
     ~PageRange();
     int insert(RID& new_rid, const std::vector<int>& columns);
-    int update(RID& rid, RID& rid_new, const std::vector<int>& columns, const std::map<int, RID>& page_directory, std::shared_mutex& lock);
+    int update(RID& rid, RID& rid_new, const std::vector<int>& columns, const std::map<int, RID>& page_directory, std::shared_mutex* lock);
     // int update(RID& rid, RID& rid_new, const std::vector<int>& columns, const std::map<int, RID>& page_directory, std::shared_lock<std::shared_mutex>* lock);
     bool base_has_capacity () const;
     int write(FILE* fp);
