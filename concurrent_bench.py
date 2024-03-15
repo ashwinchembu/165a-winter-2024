c7797ca6563f40fb5ca76a21d428290cad7aad13
zhao-18 <zhao1455@gmail.com>
2024-03-15 10:58:40 -0700

    huh

--- concurrent_bench.py
+++ concurrent_bench.py
@@ -13,8 +13,8 @@ query = Query(grades_table)
 keys = []
 records = {}
 num_threads = 8
-number_of_records = 10000
-number_of_transactions = 1000
+number_of_records = 1000
+number_of_transactions = 100
 
 
 insert_transactions = []
