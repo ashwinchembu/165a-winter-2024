2938c9aba5db204bc95a352434c5bdc5ddad981f
zhao-18 <zhao1455@gmail.com>
2024-03-15 11:27:44 -0700

    Change

---
+++ bench.sh
@@ -0,0 +1,15 @@
+if [ $1 -eq 1 ] then
+    make clean
+    make optimized
+fi
+
+make a
+python3 concurrent_bench.py
+make a
+python3 concurrent_bench.py
+make a
+python3 concurrent_bench.py
+make a
+python3 concurrent_bench.py
+make a
+python3 concurrent_bench.py
