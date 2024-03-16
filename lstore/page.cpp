e5b7f6b43741ffdf146cb39114b3238267a5e0d9
zhao-18 <zhao1455@gmail.com>
2024-03-16 15:59:07 -0700

    Revert back to normal

--- lstore/page.cpp
+++ lstore/page.cpp
@@ -189,8 +189,7 @@ int PageRange::update(RID& rid, RID& rid_new, const std::vector<int>& columns, c
         rid_new.first_rid_page = rid_new.id;
 
         // Write in the metadata except for schema encoding column
-        // buffer_pool.insert_new_page(rid_new, INDIRECTION_COLUMN, latest_rid.id);
-        buffer_pool.insert_new_page(rid_new, INDIRECTION_COLUMN, rid.id);
+        buffer_pool.insert_new_page(rid_new, INDIRECTION_COLUMN, latest_rid.id);
         buffer_pool.insert_new_page(rid_new, RID_COLUMN, rid_new.id);
         buffer_pool.insert_new_page(rid_new, TIMESTAMP_COLUMN, 0);
         buffer_pool.insert_new_page(rid_new, BASE_RID_COLUMN, rid.id);
@@ -228,8 +227,7 @@ int PageRange::update(RID& rid, RID& rid_new, const std::vector<int>& columns, c
         rid_new.offset = num_slot_used_tail - 1;
 
         // Write in the metadata except for schema encoding column
-        // buffer_pool.set(rid_new, INDIRECTION_COLUMN, latest_rid.id, true);
-        buffer_pool.set(rid_new, INDIRECTION_COLUMN, rid.id, true);
+        buffer_pool.set(rid_new, INDIRECTION_COLUMN, latest_rid.id, true);
         buffer_pool.set(rid_new, RID_COLUMN, rid_new.id, true);
         buffer_pool.set(rid_new, TIMESTAMP_COLUMN, 0, true);
         buffer_pool.set(rid_new, BASE_RID_COLUMN, rid.id, true);
