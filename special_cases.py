a8fd99f5efb68ce6bbbb3f846f456a80cef467a4
zhao-18 <zhao1455@gmail.com>
2024-03-17 23:03:26 -0700

    Tester

--- special_cases.py
+++ special_cases.py
@@ -3,7 +3,7 @@ from lstore.py.query import Query
 from lstore.py.transaction import Transaction
 from lstore.py.transaction_worker import TransactionWorker
 from time import sleep
-from random import choice, randint, sample, seed
+from random import choice, randint, sample, seed, randrange
 
 db = Database()
 db.open('./ECS165')
@@ -97,7 +97,7 @@ for i in range(1,3):
     pk_transaction_workers.append(TransactionWorker())
 
 pk_transactions[0].add_query(query.update, grades_table, 92106429, [28, None, None, None, None])
-pk_transactions[1].add_query(query.update, grades_table, 92106429, [28, None, None, None, None])
+pk_transactions[1].add_query(query.select, grades_table, 92106429, 0, [1, 1, 1, 1, 1])
 
 for i in range(1,3):
     pk_transaction_workers[i-1].add_transaction(pk_transactions[i-1])
@@ -135,19 +135,69 @@ for i in range(1,3):
 
 print('test 9 finished')
 
-# transaction = Transaction()
-#
-# transaction.add_query(query.insert, grades_table, *records[key])
-# transaction.add_query(query.update, grades_table, key, *updated_columns)
-# transaction.add_query(query.select, grades_table, key, column, [1, 1, 1, 1, 1])
-# transaction.add_query(query.select_version, grades_table, key, column, [1, 1, 1, 1, 1], version)
-# transaction.add_query(query.sum, grades_table, start, end, column)
-# transaction.add_query(query.sum_version, grades_table, start, end, column, version)
-#
-# transaction_worker = TransactionWorker()
-# transaction_worker.add_transaction(transaction)
-#
-# transaction_worker.run()
-# transaction_worker.join()
+#test 10: Change on one thread depend on other thread
+dw_transactions = []
+dw_transaction_workers = []
+
+for i in range(1,3):
+    dw_transactions.append(Transaction())
+
+for i in range(1,3):
+    dw_transaction_workers.append(TransactionWorker())
+
+dw_transactions[0].add_query(query.update, grades_table, 92106429, [28, None, None, None, None])
+dw_transactions[1].add_query(query.update, grades_table, 28, [92106429, None, None, None, None])
+
+for i in range(1,3):
+    dw_transaction_workers[i-1].add_transaction(pk_transactions[i-1])
+
+for i in range(1,3):
+    dw_transaction_workers[i-1].run()
+
+for i in range(1,3):
+    dw_transaction_workers[i-1].join()
+
+record = query.select(28, 0, [1, 1, 1, 1, 1])
+print('select on', 28, ': ', [x.columns for x in record])
+record = query.select(92106429, 0, [1, 1, 1, 1, 1])
+print('select on', 92106429, ': ', [x.columns for x in record])
+
+print('test 10 finished')
+
+transactions_select = []
+transactions_insert = []
+transactions_update = []
+transaction_workers = []
+
+for i in range(0, 20):
+    transactions_select.append(Transaction())
+    transactions_insert.append(Transaction())
+    transactions_update.append(Transaction())
+
+for i in range(0, 20):
+    ts = transactions_select[i]
+    ti = transactions_insert[i]
+    tu = transactions_update[i]
+    for j in range(0, 20):
+        ts.add_query(query.select, grades_table, 92106429 + randrange(0, 100), 0, [1, 1, 1, 1, 1])
+        tu.add_query(query.update, grades_table, 92106429 + randrange(0, 100), [None, 22, 23, 24, 25])
+        key += 1
+        ti.add_query(query.insert, grades_table, [key, 55, 56, 57, 58])
+
+transaction_workers = []
+for i in range(0,3):
+    transaction_workers.append(TransactionWorker())
+
+for i in range(0, 20):
+    transaction_workers[0].add_transaction(transactions_select[i])
+    transaction_workers[1].add_transaction(transactions_insert[i])
+    transaction_workers[2].add_transaction(transactions_update[i])
+
+transaction_workers[0].run()
+transaction_workers[1].run()
+transaction_workers[2].run()
+transaction_workers[0].join()
+transaction_workers[1].join()
+transaction_workers[2].join()
 
 db.close()
